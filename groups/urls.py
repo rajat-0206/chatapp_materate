8b14c025ef4cc7e5ea30ff6eb4df2fd41e578da3
Rajat Shrivastava <rajat123468@gmail.com>
2021-04-14 17:01:31 +0530

    added login

--- groups/urls.py
+++ groups/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="homepage"),
-    path('addmessage',views.send,name="send message")
+    path('login',views.login_user,name="login"),
+    path('addmessage',views.send,name="send message"),
+    path("logout",views.logout_user,name="logout")
+
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,21 +1,52 @@
 from django.shortcuts import render,redirect,Http404,HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.sessions.models import Session
+from django.contrib.auth.hashers import check_password,make_password,check_password
 from .models import *
 
+
+def login_user(request):
+    if(request.user.is_authenticated):
+        return redirect("/")
+    if request.method=='POST':
+        username= request.POST['username']
+        password=request.POST['password']
+
+        user= authenticate(request,username=username,password=password)
+        if (user and str(user)!="admin"):
+                login(request,user)
+                return redirect('/')
+        else:
+            error1="invalid login details supplied!"
+            return render(request,'login.html',{'error':error1})
+    else:
+        return render(request,"login.html")
+
+
 def home(request):
-    user = request.GET["username"]
-    try:
-        groupid = request.GET["room"]
-        userobj = User.objects.get(username=user)
-        group = room.objects.filter(members=userobj)
-        current = room.objects.get(id=groupid)
-        msg = message.objects.filter(group_id=groupid)
-        return render(request,"index.html",{"user":userobj,"rooms":group,"messages":msg,"current":current})
-    except:
-        pass
-    userobj = User.objects.get(username=user)
-    group = room.objects.filter(members=userobj)
-    msg = message.objects.filter(group=group[0])
-    return render(request,"index.html",{"user":userobj,"rooms":group,"messages":msg,"current":group[0]})
+    if(request.user.is_authenticated==True):
+        user = request.user.username
+        try:
+            groupid = request.GET["room"]
+            userobj = User.objects.get(username=user)
+            group = room.objects.filter(members=userobj)
+            current = room.objects.get(id=groupid)
+            listofmembers = current.members.all()
+            if(userobj not  in listofmembers):
+                raise Http404
+            else:
+                msg = message.objects.filter(group_id=groupid)
+                return render(request,"index.html",{"user":userobj,"rooms":group,"messages":msg,"current":current})
+        except:
+            userobj = User.objects.get(username=user)
+            group = room.objects.filter(members=userobj)
+            msg = message.objects.filter(group=group[0])
+            return render(request,"index.html",{"user":userobj,"rooms":group,"messages":msg,"current":group[0]})
+    else:
+        return redirect("/login")
+
 
 def send(request):
     if(request.method=="POST"):
@@ -34,3 +65,10 @@ def send(request):
             msg = message.objects.create(group_id=group,sender_id=user,text=text)
         
         return HttpResponse("Message added")
+
+
+def logout_user(request):
+    logout(request)
+    request.session['loged'] = False
+    request.session['uesrname'] =""
+    return redirect('/')
